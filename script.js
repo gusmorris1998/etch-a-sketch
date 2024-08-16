6d4cc334b86b327468119b43c31d9a28bf3c7cdc
gusmorris1998 <gusmorris1998@gmail.com>
2024-08-16 16:04:52 +1000

    Added button to change size. Some errors here

--- script.js
+++ script.js
@@ -1,23 +1,38 @@
-const cols = []
+var n = 4
+const button = document.querySelector("button")
+const body = document.querySelector("body");
 const container = document.querySelector("#container");
+run(n)
 
-for (i=0; i<4; i++) {
-    var col = document.createElement("div")
-    col.setAttribute("class", "col")
+function run(n) {
 
-    for (j=0; j<4; j++) {
-        var square = document.createElement("div")
-        square.setAttribute("class", "square")
-        col.appendChild(square)
+    const cols = []
+
+    for (i=0; i<n; i++) {
+        var col = document.createElement("div")
+        col.setAttribute("class", "col")
+    
+        for (j=0; j<n; j++) {
+            var square = document.createElement("div")
+            square.setAttribute("class", "square")
+            col.appendChild(square)
+        }
+    
+        cols.push(col)
     }
-
-    cols.push(col)
+    cols.forEach((col) => container.appendChild(col));
+    
+    var squares = document.querySelectorAll(".square");
+    const changeColour = (e) => e.target.classList.add("hover")
+    
+    squares.forEach((square) => square.addEventListener("mouseover", (e) => changeColour(e)))
+    
+    const remove = (child) => container.removeChild(child)
+    
+    button.addEventListener("click", function() { 
+        n = prompt()
+        cols.forEach((col) => remove(col));
+        run(n)
+    })
 }
-
-cols.forEach((col) => container.appendChild(col));
-
-var squares = document.querySelectorAll(".square");
-const changeColour = (e) => e.target.classList.add("hover")
-
-squares.forEach((square) => square.addEventListener("mouseover", (e) => changeColour(e)))
 
